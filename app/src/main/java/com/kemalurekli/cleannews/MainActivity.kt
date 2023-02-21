6060a4692e8eb1f7a5d324882dc1c996c59694fa
kemalurekli <kemalurekli@gmail.com>
2023-02-21 11:06:36 +0300

    some libs version updated and di added.

--- app/src/main/java/com/kemalurekli/cleannews/MainActivity.kt
+++ app/src/main/java/com/kemalurekli/cleannews/MainActivity.kt
@@ -2,7 +2,10 @@ package com.kemalurekli.cleannews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
